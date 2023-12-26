578577d09cf890d33836645c711831c85b204378
TechnicalChora <sandeepverma1523@gmail.com>
2023-12-26 22:26:15 +0530

    Task 3 is submited

--- Demo.js
+++ Demo.js
@@ -1,3 +1,6 @@
-console.log("This is Demo JS File");
-console.log("This is Demo JS Code");
-console.log("This Is Git Repo");
+document.getElementById('main-heading').innerHTML="Fruit World";
+document.getElementById('main-heading').style.color="orange";
+document.getElementById('header').style.background="green";
+document.getElementById('header').style.borderColor = 'Orange';
+document.getElementById('basket-heading').style.color="green";
+document.getElementById('thanks').innerHTML="<p>Please visit us again</p>";
