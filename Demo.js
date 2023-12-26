4bccad795b9934c8578f4813b35c205e51e16e6b
TechnicalChora <sandeepverma1523@gmail.com>
2023-12-26 21:09:57 +0530

    Final Data Save

--- Demo.js
+++ Demo.js
@@ -1,2 +1,3 @@
+console.log("This is Demo JS File");
 console.log("This is Demo JS Code");
 console.log("This Is Git Repo");
--- app.js
+++ app.js
@@ -1,2 +1,3 @@
+console.log("This is app.js file");
 console.log("I Love Java Programming");
 console.log("This is My First JS Code");
